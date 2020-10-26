58d4dfef5dce4dbc0ef5bee96749fdce7e0c33ba
dipzera <dipzera@github.com>
2020-10-26 12:13:12 +0200

    added all view routes

--- src/router/index.js
+++ src/router/index.js
@@ -6,21 +6,58 @@ Vue.use(VueRouter);
 
 const routes = [
     {
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
-    {
         path: "/login",
         name: "login",
         meta: { layout: "empty" },
         component: () => import("../views/Login.vue"),
     },
     {
+        path: "/register",
+        name: "register",
+        meta: { layout: "empty" },
+        component: () => import("../views/Register.vue"),
+    },
+    {
+        path: "/",
+        name: "Home",
+        meta: { layout: "main" },
+        component: () => import("../views/Home.vue"),
+    },
+    {
         path: "/categories",
         name: "categories",
         meta: { layout: "main" },
         component: () => import("../views/Categories.vue"),
+    },
+    {
+        path: "/profile",
+        name: "profile",
+        meta: { layout: "main" },
+        component: () => import("../views/Profile.vue"),
+    },
+    {
+        path: "/record",
+        name: "record",
+        meta: { layout: "main" },
+        component: () => import("../views/Record.vue"),
+    },
+    {
+        path: "/detail",
+        name: "details",
+        meta: { layout: "main" },
+        component: () => import("../views/Detail.vue"),
+    },
+    {
+        path: "/planning",
+        name: "planning",
+        meta: { layout: "main" },
+        component: () => import("../views/Planning.vue"),
+    },
+    {
+        path: "/history",
+        name: "history",
+        meta: { layout: "main" },
+        component: () => import("../views/History.vue"),
     },
 ];
 
