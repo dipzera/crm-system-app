41558a9e42b75ce94dcc34400ebfe14a30bd1192
dipzera <nathanielthomaz@gmail.com>
2020-11-29 22:30:27 +0200

    added CategoryEdit component, connected it to firebase

--- src/components/CategoryEdit.vue
+++ src/components/CategoryEdit.vue
@@ -2,34 +2,126 @@
     <div class="col s12 m6">
         <div>
             <div class="page-subtitle">
-                <h4>Редактировать</h4>
+                <h4>Edit</h4>
             </div>
 
-            <form>
+            <form @submit.prevent="submitHandler">
                 <div class="input-field">
-                    <select>
-                        <option>Category</option>
+                    <select ref="select" v-model="current">
+                        <option
+                            v-for="c of categories"
+                            :key="c.id"
+                            :value="c.id"
+                        >
+                            {{ c.title }}
+                        </option>
                     </select>
-                    <label>Выберите категорию</label>
+                    <label>Select category</label>
                 </div>
 
                 <div class="input-field">
-                    <input type="text" id="name" />
-                    <label for="name">Название</label>
-                    <span class="helper-text invalid">TITLE</span>
+                    <input
+                        id="name"
+                        type="text"
+                        v-model="title"
+                        :class="{
+                            invalid: $v.title.$dirty && !$v.title.required,
+                        }"
+                    />
+                    <label for="name">Name</label>
+                    <span
+                        class="helper-text invalid"
+                        v-if="$v.title.$dirty && !$v.title.required"
+                    >
+                        Insert category name
+                    </span>
                 </div>
 
                 <div class="input-field">
-                    <input id="limit" type="number" />
-                    <label for="limit">Лимит</label>
-                    <span class="helper-text invalid">LIMIT</span>
+                    <input
+                        id="limit"
+                        type="number"
+                        v-model.number="limit"
+                        :class="{
+                            invalid: $v.limit.$dirty && !$v.limit.minValue,
+                        }"
+                    />
+                    <label for="limit">Limit</label>
+                    <span
+                        class="helper-text invalid"
+                        v-if="$v.limit.$dirty && !$v.limit.minValue"
+                    >
+                        Minimal value {{ $v.limit.$params.minValue.min }}
+                    </span>
                 </div>
 
                 <button class="btn waves-effect waves-light" type="submit">
-                    Обновить
+                    Submit
                     <i class="material-icons right">send</i>
                 </button>
             </form>
         </div>
     </div>
 </template>
+
+<script>
+import { required, minValue } from "vuelidate/lib/validators";
+export default {
+    validations: {
+        title: { required },
+        limit: { minValue: minValue(1) },
+    },
+    watch: {
+        current(catId) {
+            const { title, limit } = this.categories.find(
+                (c) => c.id === catId
+            );
+            this.limit = limit;
+            this.title = title;
+        },
+    },
+    created() {
+        const { title, id, limit } = this.categories[0];
+        this.current = id;
+        this.title = title;
+        this.limit = limit;
+    },
+    props: {
+        categories: {
+            type: Array,
+            required: true,
+        },
+    },
+    data: () => ({
+        select: null,
+        title: "",
+        limit: 1,
+        current: null,
+    }),
+    mounted() {
+        M.updateTextFields();
+        this.select = M.FormSelect.init(this.$refs.select);
+    },
+    destroyed() {
+        if (this.select && this.select.destroy) {
+            this.select.destroy();
+        }
+    },
+    methods: {
+        async submitHandler() {
+            if (this.$v.$invalid) {
+                this.$v.$touch();
+                return;
+            }
+            const categoryData = {
+                id: this.current,
+                title: this.title,
+                limit: this.limit,
+            };
+            await this.$store.dispatch("updateCategory", categoryData);
+            this.$message("Category successfully updated!");
+            this.$emit("updated", categoryData);
+        },
+    },
+};
+</script>
--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,9 +4,16 @@
             <h3>Categories</h3>
         </div>
         <section>
-            <div class="row">
+            <Loader v-if="loading" />
+            <div class="row" v-else>
                 <CategoryCreate @created="addNewCategory" />
-                <CategoryEdit />
+                <CategoryEdit
+                    v-if="categories.length"
+                    :categories="categories"
+                    :key="this.categories.length + updateCount"
+                    @updated="updateCategories"
+                />
+                <p v-else class="center">No categories yet!</p>
             </div>
         </section>
     </div>
@@ -18,8 +25,14 @@ import CategoryEdit from "@/components/CategoryEdit";
 export default {
     name: "categories",
     data: () => ({
-        caetgories: [],
+        categories: [],
+        loading: true,
+        updateCount: 0,
     }),
+    async mounted() {
+        this.categories = await this.$store.dispatch("fetchCategories");
+        this.loading = false;
+    },
     components: {
         CategoryCreate,
         CategoryEdit,
@@ -27,7 +40,12 @@ export default {
     methods: {
         addNewCategory(category) {
             this.categories.push(category);
-            console.log(this.categories);
+        },
+        updateCategories(category) {
+            const idx = this.categories.findIndex((c) => c.id === category.id);
+            this.categories[idx].title = category.title;
+            this.categories[idx].limit = category.limit;
+            this.updateCount++;
         },
     },
 };
